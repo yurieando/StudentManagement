aae354a7bb5c8f625fed9b5887c2763cf35cc817
Yurie Ando <yu.lily.au13@gmail.com>
2024-11-18 09:43:14 +0900

    例外処理の実装

--- src/main/java/raisetech/student/management/domain/StudentDetail.java
+++ src/main/java/raisetech/student/management/domain/StudentDetail.java
@@ -1,5 +1,6 @@
 package raisetech.student.management.domain;
 
+import jakarta.validation.Valid;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -15,6 +16,9 @@ import raisetech.student.management.data.StudentCourse;
 
 public class StudentDetail {
 
+  @Valid
   private Student student;
+
+  @Valid
   private List<StudentCourse> studentCourseList;
 }
