b44fd7bc1bfe2c9d751d03cdff43b4ee47f37ce4
Yurie Ando <yu.lily.au13@gmail.com>
2025-01-08 12:15:44 +0900

    Converterのテストコードを実装しました。

--- src/main/java/raisetech/student/management/controller/converter/StudentConverter.java
+++ src/main/java/raisetech/student/management/controller/converter/StudentConverter.java
@@ -15,14 +15,15 @@ import raisetech.student.management.domain.StudentDetail;
 public class StudentConverter {
 
   /*
-    * 受講生情報と受講生コース情報を結合します。
-    * 受講生コース上hじょうは受講生に対して複数存在するので、ループを回して受講生詳細情報を組み立てる。
-    *
-    * @param studentList 受講生一覧
-    * @param studentsCourses 受講生コース情報一覧
-    * @return 受講生詳細情報一覧
+   * 受講生情報と受講生コース情報を結合します。
+   * 受講生コース情報は受講生に対して複数存在するので、ループを回して受講生詳細情報を組み立てる。
+   *
+   * @param studentList 受講生一覧
+   * @param studentsCourses 受講生コース情報一覧
+   * @return 受講生詳細情報一覧
    */
-  public List<StudentDetail> convertStudentDetails(List<Student> studentList, List<StudentCourse> studentCourseList) {
+  public List<StudentDetail> convertStudentDetails(List<Student> studentList,
+      List<StudentCourse> studentCourseList) {
     List<StudentDetail> studentDetails = new ArrayList<>();
     studentList.forEach(student -> {
       StudentDetail studentDetail = new StudentDetail();
